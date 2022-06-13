a8b9324ddf33755240ed1e2799a1aaaca3def180
Claudia Ramirez <mely.ramiz07@gmail.com>
2022-06-12 21:33:22 -0500

    Feat: Add function addition

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,27 @@ import "./App.css";
 
 function App() {
 
+  const [number, setNumber] = useState({one: 0, two: 0})
+  const [suma, setSuma] = useState()
+
+
+  const handleInput = (event) => {
+    const { name, value } = event.target
+    setNumber({...number, [name]: value})
+  }
+
   return (
     <div className="row">
       <div className="col-4">
-      <input type="number" name="numberOne"></input>
+      <input className="input" onChange={handleInput} name="one" value={number.one} placeholder="Ingesa un numero"/>
+      <input className="input" onChange={handleInput} name="two" value={number.two} placeholder="Ingesa un numero"/>
+      <input value={suma}/>
       </div>
       <div className="col-4">
-      <input type="number" name="numberTwo"></input>
-      </div>
-      <div className="col-4">
-      <button name="btnSumar" >Sumar</button>
+      <button name="btnSumar" onClick={()=>{
+        const { one, two } = number
+        setSuma(Number(one) + Number(two))
+      }}>Sumar</button>
       </div>
       <div className="col-4">
       <button name="btnRestar">Restar</button>
