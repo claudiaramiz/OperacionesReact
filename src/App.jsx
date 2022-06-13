4e598e61f90f3608fa1cf2f673a1a585b2b80e67
Claudia Ramirez <mely.ramiz07@gmail.com>
2022-06-12 21:13:29 -0500

    Feat: Add file for the calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,188 +1,35 @@
-  /* import React, { useState } from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
 
-const [counter, setCounter] = useState(0);
-
-  const [person, setPerson] = useState({
-
-    name: "John",
-
-    lastName: "Doe",
-
-    age: 30,
-
-    favouriteFood: "Pizza",
-
-    favouriteColour: "Red",
-
-    counter: 0,
-
-  });
-
-  return (
-
-    <div className="App">
-
-      <h1 className="text-blue">{`${person.name} ${person.lastName}`}</h1>
-
-      <h1 className="text-blue">{`age ${person.age}`}</h1>
-
-      <h1 className="text-blue">{`favouriteFood ${person.favouriteFood}`}</h1>
-
-      <h1 className="text-blue">{`favouriteColour ${person.favouriteColour}`}</h1>
-
-      {/* <input type="number" name="firstNumber" />
-
-      <input type="number" name="secondNumber" /> */}
-/*
-      <button
-
-        onClick={() => {
-
-          setPerson((current) => ({
-
-            ...current,
-
-            counter: current.counter + 1,
-
-          }));
-
-        }}
-
-      >
-
-        Sumar
-
-      </button>
-
-      <button
-
-        onClick={() => {
-
-          setPerson((current) => ({
-
-            ...current,
-
-            counter: current.counter - 1,
-
-          }));
-
-        }}
-
-      >
-
-        Restar
-
-      </button>
-
-      <h1 className="text-red">counter: {person.counter}</h1>
-
-    </div>
-
-  );
-
-}
-
-
-
-export default App;
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*/
-
-
-import React, { useState } from "react";
-import "./App.css";
-
-  function App() {
-
-  const [people, setPeople] = useState([
-    {
-      name: "John",
-      lastName: "Doe",
-      age: 30,
-      favouriteFood: "Pizza",
-      favouriteColour: "Red",
-      counter: 0,
-    },
-  ]);
-
-  const [name, setName] = useState("");
-  const [lastName, setLastname] = useState("");
-
   return (
-
-    <div className="App">
-      <div className="col-lg-4">
+    <div className="row">
+      <div className="col-4">
       <input type="number" name="numberOne"></input>
       </div>
-
-      <div className="col-lg-4">
+      <div className="col-4">
       <input type="number" name="numberTwo"></input>
-        </div>
-
-        <div className="col-lg-4">
-        <button
-        onClick={() => {
-          sumar((current) => [{ numberOne, numberTwo }, ...current]);
-        }}
-      >
-        Sumar
-      </button>
-      <button
-        onClick={() => {
-          restar((current) => [{ numberOne, numberTwo }, ...current]);
-        }}
-      >
-        Restar
-      </button>
-      <button
-        onClick={() => {
-          dividir((current) => [{ numberOne, numberTwo }, ...current]);
-        }}
-      >
-        Dividir
-      </button>
-      <button
-        onClick={() => {
-          multiplicar((current) => [{ numberOne, numberTwo }, ...current]);
-        }}
-      >
-        Multiplicar
-      </button>
-        </div>
-
-      <input
-        type="text"
-        name="name"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-      <input
-        type="text"
-        name="lastName"
-        onChange={(event) => {
-          setLastname(event.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          setPeople((current) => [{ name, lastName }, ...current]);
-        }}
-      >
-        Add user
-      </button>
-      <ul>
-        {people.map((person, idx) => (
-          <li id={idx}>{`${person.name} ${person.lastName}`}</li>
-        ))}
-      </ul>
+      </div>
+      <div className="col-4">
+      <button name="btnSumar" >Sumar</button>
+      </div>
+      <div className="col-4">
+      <button name="btnRestar">Restar</button>
+      </div>
+      <div className="col-4">
+      <button name="btnMultiplicar">Multiplicar</button>
+      </div>
+      <div className="col-4">
+      <button name="btnDividir">Dividir</button>
+      </div>
+      <div className="col-4">
+      <label>Total</label>
+      </div>
     </div>
+  )
 
-  );
 }
 
 export default App;
--- src/calculadora.js
+++ src/calculadora.js
@@ -8,7 +8,7 @@ var Screen = React.createClass({
       );
     }
   });
-
+  
   var Inputs = React.createClass({
     render: function(){
       return (
@@ -63,5 +63,90 @@ var Screen = React.createClass({
     }
   });
   
-
+  var Button = React.createClass({
+    render: function(){
+      var classes = ['defaultButton'];
+      classes.push(this.props.className);
+      return (
+        <button type="button" className={classes.join(' ')} onClick={(e) => this.props.onClick(e, this.props.label)}>
+          {this.props.label}
+        </button>
+      );
+    }
+  });
+  
+  var Container = React.createClass({
+    getInitialState: function(){
+      return {
+        operando: '',
+        operador: '',
+        display: '0',
+        resultDisplayed: false
+      }
+    },
+    onClickOperando: function(e, label){
+      if(this.state.display == '0' || isNaN(this.state.display) || this.state.resultDisplayed){
+        var display = label;
+        this.setState({ resultDisplayed: false, });
+      } else {
+        var display = this.state.display + label;
+      }
+      
+      this.setState({ display: display });
+    },
+    onClickOperador: function(e, label){
+      if(this.state.operador.length > 0){
+        this.calculate();
+        if(label == '='){
+          this.setState({
+            resultDisplayed: true,
+            operador: ''
+          });
+        } else {
+          this.setState({
+            resultDisplayed: true,
+            operador: label
+          });
+        }
+      } else {
+        this.setState({
+          operando: this.state.display,
+          operador: label,
+          display: label
+        });
+      }
+    },
+    onClickClean: function(){
+      this.setState(this.getInitialState);
+    },
+    calculate: function(){var displayResult;
+        switch(this.state.operador){
+          case '+':
+            displayResult = parseFloat(this.state.operando) + parseFloat(this.state.display);
+            break;
+          case '-':
+            displayResult = parseFloat(this.state.operando) - parseFloat(this.state.display);
+            break;
+          case '/':
+            displayResult = parseFloat(this.state.operando) / parseFloat(this.state.display);
+            break;
+          case 'x':
+            displayResult = parseFloat(this.state.operando) * parseFloat(this.state.display);
+            break;
+        }
+        this.setState({
+          operando: displayResult,
+          display: displayResult
+        });
+    },
+    render: function() {
+      return (
+        <div id="container">
+          <Screen displayText={this.state.display} onClickClean={this.onClickClean}/>
+          <Inputs onClickOperando={this.onClickOperando} onClickOperador={this.onClickOperador}/>
+        </div>
+      );
+    }
+  });
+  
   React.render(<Container/>, document.getElementById('calculadora'));
