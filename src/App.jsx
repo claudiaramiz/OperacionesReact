3308d7d852015ede45462604498810d3d7340f48
Claudia Ramirez <mely.ramiz07@gmail.com>
2022-06-12 22:05:11 -0500

    Feat: Modify style

--- src/App.jsx
+++ src/App.jsx
@@ -16,37 +16,40 @@ function App() {
 
   return (
     <div className="card">
+      <div className="card-header"><h1>Operaciones básicas</h1></div>
       <div className="card-body">
       <input className="input" onChange={handleInput} name="one" value={number.one} placeholder="Ingesa un numero"/>
       </div>
       <div className="card-body">
       <input className="input" onChange={handleInput} name="two" value={number.two} placeholder="Ingesa un numero"/>
       </div>
-      <div className="col-4">
+      <div className="card-button">
       <button name="btnSumar" onClick={()=>{
         const { one, two } = number
         setSuma(Number(one) + Number(two))
       }}>Sumar</button>
       </div>
-      <div className="col-4">
+      <div className="card-button">
       <button name="btnRestar"onClick={()=>{
         const { one, two } = number
         setSuma(Number(one) - Number(two))
       }}>Restar</button>
       </div>
-      <div className="col-4">
+      <div className="card-button">
       <button name="btnMultiplicar"onClick={()=>{
         const { one, two } = number
         setSuma(Number(one) * Number(two))
       }}>Multiplicar</button>
       </div>
-      <div className="col-4">
+      <div className="card-button">
       <button name="btnDividir"onClick={()=>{
         const { one, two } = number
         setSuma(Number(one) / Number(two))
       }}>Dividir</button>
       </div>
-      <div className="card-body"><input value={suma}/></div>
+      <div className="card-body">
+      <h5>Resultado</h5><input value={suma}/>
+        </div>
       
     </div>
   )
